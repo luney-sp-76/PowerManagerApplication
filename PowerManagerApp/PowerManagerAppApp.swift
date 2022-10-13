48a66bd75850af0d01a84a3629611c327315e2f9
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-10-13 10:21:32 +0100

    Initial Commit

---
+++ PowerManagerApp/PowerManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerManagerAppApp.swift
+//  PowerManagerApp
+//
+//  Created by Paul Olphert on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PowerManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
